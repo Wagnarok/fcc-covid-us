aba85d2607cedda200171c1944a0449abfd83cec
Wagnarok <jbenwagner@gmail.com>
2020-08-13 07:53:35 -0400

    about page basic setup

---
+++ src/routes/about.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <title>About Covid-19 Tracker US</title>
+</svelte:head>
+
+<div class="section header">
+  <div class="container">
+    <h1>About</h1>
+    <p>The data in this site</p>
+  </div>
+</div>
